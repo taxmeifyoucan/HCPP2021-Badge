69a930cedc796c70c8cafaed1b7a607a023d3b26
Mário Havel <61149543+taxmeifyoucan@users.noreply.github.com>
2022-05-19 23:52:40 +0100

    Update gen_pairs.sh
    Fix broken api

--- gen_pairs.sh
+++ gen_pairs.sh
@@ -17,24 +17,27 @@ ID=`curl -s -X POST https://$4/usermanager/api/v1/users \
 -d '{"admin_id": "'"$1"'", "wallet_name": "'"$WALL"'", "user_name": "'"$USER"'", "email": "", "password": ""}' \
 -H "X-Api-Key: $2" -H "Content-type: application/json" | tee -a wallets | jq -r '.id'`
 
-AKEY=`grep $ID wallets -m 1 -A  6 | grep "adminkey" | awk -F'"' '{ print $4 }'`
-INKEY=`grep $ID wallets -m 1 -A 8 | grep '"inkey"' | awk -F'"' '{ print $4 }'`
+AKEY=`cat wallets | jq | grep $ID -A 2 | grep '"adminkey"' | awk -F'"' '{ print $4 }'`
+INKEY=`cat wallets | jq | grep $ID -A 2 | grep '"inkey"' | awk -F'"' '{ print $4 }'`
 
-curl -s -X POST https://$4/api/v1/extensions \
+curl -s -X POST https://$4/usermanager/api/v1/extensions \
  -d '{"userid": "'"$ID"'", "extension": "withdraw", "active": true}' \
  -H "X-Api-Key: $INKEY" -H "Content-type: application/json" >> /dev/null
 
 curl -s -X POST https://$4/withdraw/api/v1/links \
- -d '{"title": "'"$INKEY"'", "min_withdrawable": 1, "max_withdrawable": 100000, "uses": 100, "wait_time": 10, "is_unique": false}' \
+ -d '{"title": "'"$INKEY"'", "min_withdrawable": 10, "max_withdrawable": 100000, "uses": 100, "wait_time": 10, "is_unique": false}' \
  -H "X-Api-Key: $AKEY" -H "Content-type: application/json" | jq -r '.lnurl' | tee -a badges
 
 curl -s -X POST https://$4/usermanager/api/v1/extensions \
  -d '{"userid": "'"$ID"'", "extension": "lnurlp", "active": true}' \
  -H "X-Api-Key: $INKEY" -H "Content-type: application/json" >> /dev/null
 
-curl -s -X POST https://$4/lnurlp/api/v1/links \
- -d '{"description": "LNURL Badge", "amount": 1, "max": 4000000, "min": 1, "comment_chars": 10}' \
- -H "X-Api-Key: $INKEY" -H "Content-type: application/json" | jq -r '.lnurl' | tee -a badges
+LNURID=`curl -s -X POST https://$4/lnurlp/api/v1/links \
+ -d '{"description": "Pizza Day Badge", "amount": 1, "max": 4000000, "min": 1, "comment_chars": 10}' \
+ -H "X-Api-Key: $INKEY" -H "Content-type: application/json" | jq -r '.id'`
+
+curl -s -X GET https://$4/lnurlp/api/v1/links/$LNURID -H "X-Api-Key: $INKEY" | jq '.lnurl' | tr -d '"' | tee -a badges 
+
 
 echo https://$4/wallet?usr=$ID | tee -a badges
 
